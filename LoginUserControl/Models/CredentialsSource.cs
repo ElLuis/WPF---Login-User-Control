5b6f1b1ec8bb4bdb3a56806434fc1682902ce780
Luis Sanchez <lsanch14@my.centennialcollege.ca>
2018-02-26 21:41:25 -0500

    Implemented view model and properties to maintain the login credentials.

---
+++ LoginUserControl/Models/CredentialsSource.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginUserControl.Models
+{
+    public class CredentialsSource
+    {
+        private string password, username;
+
+        //Properties
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                password = value;
+            }
+        }
+
+
+        public string Username
+        {
+            get
+            {
+                return username;
+            }
+            set
+            {
+                username = value;
+            }
+        }
+
+        public CredentialsSource(string _username, string _password)
+        {
+            this.username = _username;
+            this.password = _password;
+        }
+    }
+}
--- LoginUserControl/UserControls/LoginView.xaml.cs
+++ LoginUserControl/UserControls/LoginView.xaml.cs
@@ -20,9 +20,29 @@ namespace LoginUserControl.UserControls
     /// </summary>
     public partial class LoginUC : UserControl
     {
+        private static List<Models.CredentialsSource> credentials;
         public LoginUC()
         {
             InitializeComponent();
         }
-    }
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            passwordTB.Password = String.Empty;
+            userNameTB.Text = String.Empty;
+        }
+
+        private void loginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string password = passwordTB.Password.ToString();
+            string username = userNameTB.Text;
+            credentials.Add(new Models.CredentialsSource(username,password));
+        }
+
+        public static List<Models.CredentialsSource> GetCredentials()
+        {
+            var result = credentials.ToList<Models.CredentialsSource>();
+            return result;
+        }
+     } 
 }
